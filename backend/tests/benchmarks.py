6e6cad7808d03eda3eb04ab4f93195d19f9e2a7b
Diego Canez <canezdiego@gmail.com>
2021-08-05 23:35:20 -0500

    add more precise benchmark

--- backend/tests/benchmarks.py
+++ backend/tests/benchmarks.py
@@ -17,20 +17,22 @@ def random_points(n: int, d: int) -> List[Point]:
     return [random_point(d) for _ in range(n)]
 
 
-def rtree_knn(N: int):
-    D = 128
-    K = 8
-    points = random_points(N, D)
-
-    rtree = RTree(D, False)
-    rtree.build(points)
-
-    c = random_point(D)
-    rtree.knn(c, K, False)
+def rtree_knn(rtree):
+    c = random_point(128)
+    rtree.knn(c, 8, False)
 
 
 def test_benchmark10(benchmark):
-    benchmark(rtree_knn, 10)
+    N = 10
+    points = random_points(N, 128)
+    rtree = RTree(128, False)
+    rtree.build(points)
+    benchmark(rtree_knn, rtree)
+
 
 def test_benchmark100(benchmark):
-    benchmark(rtree_knn, 100)
+    N = 100
+    points = random_points(N, 128)
+    rtree = RTree(128, False)
+    rtree.build(points)
+    benchmark(rtree_knn, rtree)
